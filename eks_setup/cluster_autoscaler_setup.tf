b12a5d8f7d9718f8bfed9310e0f2042d1309f730
Havryliuk-Pavlo-Maksymyliano <antihetmandb@gmail.com>
2025-11-16 00:23:52 +0200

    TAT-83: added helm chart for cluster autoscaling

---
+++ eks_setup/cluster_autoscaler_setup.tf
@@ -0,0 +1,81 @@
+data "aws_iam_policy_document" "cluster_autoscaler_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "cluster_autoscaler_role" {
+  name               = "${aws_eks_cluster.eks.name}-cluster-autoscaler"
+  assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler_document.json
+}
+
+resource "aws_iam_policy" "cluster_autoscaler_policy" {
+  name = "${aws_iam_role.cluster_autoscaler_role.name}-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeScalingActivities",
+          "ec2:DescribeImages",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:GetInstanceTypesFromInstanceRequirements",
+          "eks:DescribeNodegroup"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler_policy_attachment" {
+  role       = aws_iam_role.cluster_autoscaler_role.name
+  policy_arn = aws_iam_policy.cluster_autoscaler_policy.arn
+
+}
+
+resource "aws_eks_pod_identity_association" "cluster_autoscaler" {
+  cluster_name    = aws_eks_cluster.eks.name
+  namespace       = "kube-system"
+  service_account = "cluster-autoscaler"
+  role_arn        = aws_iam_role.cluster_autoscaler_role.arn
+}
+
+resource "helm_release" "cluster_autoscaler" {
+  name = "autoscaler"
+
+  repository = "https://github.com/The-A-Team-organization/illuminati_eks"
+  chart      = "cluster_autoscaler"
+  namespace  = "kube-system"
+
+  set = [{
+    name  = ".Values.serviceAccount"
+    value = aws_eks_pod_identity_association.cluster_autoscaler.service_account
+    },
+
+    {
+      name  = ".Values.clusterName"
+      value = aws_eks_cluster.eks.name
+  }]
+}
--- eks_setup/main.tf
+++ eks_setup/main.tf
@@ -2,11 +2,26 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config" # Path to the kubeconfig file
+  }
+}
+
+
 terraform {
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "6.14.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
   }
 }
